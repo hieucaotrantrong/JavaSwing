3bf1b90107ff83e8878abe5b546c67a6298a447f
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-08-10 15:13:11 +0700

    javaswing

---
+++ Buoi1/Vidu.java
@@ -0,0 +1,15 @@
+package Buoi1;
+
+import javax.swing.JFrame;
+
+/*
+ |---------------------------------------
+ Bài 78 : Tìm hiểu về Jfame trong java
+ |---------------------------------------
+ */
+public class Vidu {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+    }
+}
